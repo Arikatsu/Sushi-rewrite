6e2326e756e8bfea716e1f483abdb37671b075fc
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-06-19 09:32:33 +0530

    kissy uwu

--- commands/fun/kiss.js
+++ commands/fun/kiss.js
@@ -4,14 +4,14 @@ const random = require("something-random-on-discord").Random
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('hug')
-        .setDescription('Hug someone in the server')
-        .addUserOption(option => option.setName('user').setDescription('user to hug').setRequired(true)),
+        .setName('kiss')
+        .setDescription('Kiss someone in the server')
+        .addUserOption(option => option.setName('user').setDescription('user to kiss').setRequired(true)),
     async execute(interaction) {
-        let data = await random.getAnimeImgURL("hug")
+        let data = await random.getAnimeImgURL("kiss")
         let user = interaction.options.getUser('user')
         let embed = new MessageEmbed()
-        .setDescription(`${interaction.user.username} hugs ${user.username}`)
+        .setDescription(`${interaction.user.username} kisses ${user.username}`)
         .setColor('RANDOM')
         .setImage(data)
         await interaction.reply({embeds: [embed]})
