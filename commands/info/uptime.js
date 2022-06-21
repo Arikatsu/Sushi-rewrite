d04f18e9b01570d1efaca33b3ad2959e8ac82979
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-06-21 07:06:01 +0530

    additions and changes as usual

---
+++ commands/info/uptime.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('uptime')
+        .setDescription('Gives the bot\'s uptime'),
+    async execute(interaction, client) {
+        let days = Math.floor(client.uptime / 86400000);
+        let hours = Math.floor(client.uptime / 3600000) % 24;
+        let minutes = Math.floor(client.uptime / 60000) % 60;
+        let seconds = Math.floor(client.uptime / 1000) % 60;
+
+        let embed = new MessageEmbed()
+            .setTitle('**Sushi\'s Uptime**')
+            .setDescription(`${days}d ${hours}h ${minutes}m ${seconds}s`)
+            .setTimestamp()
+        
+        interaction.reply({embeds: [embed]})
+    }
+}
