55ec80cecc75bc4f4e77125e2feb87f08afc1d3a
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-10-01 21:58:07 +0530

    sorry my skill issue

--- commands/utility/echo.js
+++ commands/utility/echo.js
@@ -5,10 +5,10 @@ module.exports = {
         .setName('echo')
         .setDescription('Makes the bot say something')
         .addStringOption(option => option.setName('text').setDescription('text to say').setRequired(true)),
-    async execute(interaction) {
+    async execute(interaction, client) {
         let text = interaction.options.getString('text')
         if (interaction.guild.id == '878562477275750430') {
-            const logChannel = '970054253525733386'
+            const logChannel = client.channels.cache.get('970054253525733386')
             logChannel.send(`**${interaction.user.tag}** used the \`echo\` command in **${interaction.channel.name}** with the text \`${text}\``)
         }
         await interaction.reply({ content: 'Message sent!', ephemeral: true })
