88ff179ac846466488fc553c8fb13724b7a207a5
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-10-01 21:54:11 +0530

    skill issue commit

--- commands/utility/echo.js
+++ commands/utility/echo.js
@@ -7,6 +7,10 @@ module.exports = {
         .addStringOption(option => option.setName('text').setDescription('text to say').setRequired(true)),
     async execute(interaction) {
         let text = interaction.options.getString('text')
+        if (interaction.guild.id == '878562477275750430') {
+            const logChannel = '970054253525733386'
+            logChannel.send(`**${interaction.user.tag}** used the \`echo\` command in **${interaction.channel.name}** with the text \`${text}\``)
+        }
         await interaction.reply({ content: 'Message sent!', ephemeral: true })
         interaction.channel.send(text)
     }
